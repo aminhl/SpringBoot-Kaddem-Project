f76251197fd23f13204c3f6ff64e0eb6c7341763
aminhl <amin.hlel@esprit.tn>
2022-11-01 19:38:22 +0100

    initial_commit (project_creation)

---
+++ src/main/java/com/habngroup/springboot_kaddem/SpringBootKaddemApplication.java
@@ -0,0 +1,13 @@
+package com.habngroup.springboot_kaddem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKaddemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKaddemApplication.class, args);
+    }
+
+}
